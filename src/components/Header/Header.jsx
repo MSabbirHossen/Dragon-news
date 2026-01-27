7615189edb424daa3583d8ab3b5d95237979ab54
MSabbirHossen <mshossen724@gmail.com>
2026-01-27 20:40:43 +0300

    Daynamic dated added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import logo from '../../assets/logo.png';
+import { format } from "date-fns";
+import logo from "../../assets/logo.png";
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center">
-        <img src={logo} className="w-[400px]" alt="Dragon News Logo" />
-        <p className="text-accent">Journalism Without Fear or Favour</p>
-        <p>Sunday, November 27, 2025</p>
+      <img src={logo} className="w-[400px]" alt="Dragon News Logo" />
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      {/* <p>Sunday, November 27, 2025</p> */}
+      <p>{format(new Date(), "EEEE, ")}
+        <span className="text-accent">
+      {format(new Date(), " MMMM dd, yyyy")}</span>
+      </p>
     </div>
   );
 };
